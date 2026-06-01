ca1cfb00aeb88ed2e2c2656d45a025a1581caaf4
!="Anonymous <236368458+UntitledDistribution@users.noreply.github.com>
2026-06-01 09:58:38 +0100

    Initial Release of DigitalCV Code

---
+++ script.js
@@ -0,0 +1,5 @@
+function showExtraInfo() {
+    // Wrap the HTML in single quotes to make it a valid string - Double quotes will not work as they are used inside the string
+    document.getElementById("extra-info").innerHTML = 
+        '<ul class="custom-list"><li>Digital CV</li><li>User Portal System</li></ul>';
+}
